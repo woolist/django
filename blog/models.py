78e87861dc8cf026bba3f7579d2a441543abec54
woolist <gusdnekd47@gmail.com>
2023-02-08 00:39:37 +0000

    django_tutorial

--- blog/models.py
+++ blog/models.py
@@ -1,75 +1,75 @@
 import os
 from django.db import models
-from django.contrib.auth.models import User
-from markdownx.models import MarkdownxField
-from markdownx.utils import markdown
+# from django.contrib.auth.models import User
+# from markdownx.models import MarkdownxField
+# from markdownx.utils import markdown
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+# class Tag(models.Model):
+#     name = models.CharField(max_length=50)
+#     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    def get_absolute_url(self):
-        return f'/blog/tag/{self.slug}/'
+#     def get_absolute_url(self):
+#         return f'/blog/tag/{self.slug}/'
         
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+# class Category(models.Model):
+#     name = models.CharField(max_length=50, unique=True)
+#     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    def get_absolute_url(self):
-        return f'/blog/category/{self.slug}/'
+#     def get_absolute_url(self):
+#         return f'/blog/category/{self.slug}/'
 
-    class Meta:
-        verbose_name_plural = 'categories'
+#     class Meta:
+#         verbose_name_plural = 'categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    hook_text = models.CharField(max_length=100, blank=True)
-    content = MarkdownxField()
+    # hook_text = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
 
-    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+    # head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    # file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    # author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     
-    tags = models.ManyToManyField(Tag, blank=True)
+    # tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):        
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}' # pk는 자동으로 생성되는 번호
 
     def get_absolute_url(self):
-        return f'/blog/{self.pk}/'
+        return f'/blog/{self.pk}/' # url만들어줌 >> blog/1 , blog/2
 
-    def get_file_name(self):
-        return os.path.basename(self.file_upload.name)
+#     def get_file_name(self):
+#         return os.path.basename(self.file_upload.name)
 
-    def get_file_ext(self):
-        return self.get_file_name().split('.')[-1]
+#     def get_file_ext(self):
+#         return self.get_file_name().split('.')[-1]
 
-    def get_content_markdown(self):
-        return markdown(self.content)
+#     def get_content_markdown(self):
+#         return markdown(self.content)
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     modified_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f'{self.author}::{self.content}'
+#     def __str__(self):
+#         return f'{self.author}::{self.content}'
 
-    def get_absolute_url(self):
-        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+#     def get_absolute_url(self):
+#         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
 # Create your models here.
